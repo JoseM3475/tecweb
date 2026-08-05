9fddd5be318237e03cc5700d6a820a428e1da761
itmweb <itmwebhub@gmail.com>
2026-08-05 10:03:26 +0000

    Cambio de comando color

--- itm/ejercicios-javascript/code/estructura-switch-cadena.js
+++ itm/ejercicios-javascript/code/estructura-switch-cadena.js
@@ -1,12 +1,20 @@
 var col;
-col=prompt ("ingrese el color que quiera en el fondo de ventana (rojo,verde,azul)", "");
-switch (col)
-{   
- case "rojo": document.bgColor="#ff0000";
-         break;  
- case "verde": document.bgColor="#00ff00";
-         break;  
- case "azul": document.bgColor="#0000ff";
-         break;  
- default:document.write( "Debe ingresar un color RGB");
+
+col = prompt("Ingrese el color que quiera en el fondo de ventana (rojo, verde, azul)", "");
+
+switch (col) {   
+    case "rojo":
+        document.body.style.backgroundColor = "#ff0000";
+        break;
+
+    case "verde":
+        document.body.style.backgroundColor = "#00ff00";
+        break;
+
+    case "azul":
+        document.body.style.backgroundColor = "#0000ff";
+        break;
+
+    default:
+        document.write("Debe ingresar un color RGB");
 }
