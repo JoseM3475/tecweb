bf0756f23589e3dd9412854e85112040049f3b5e
itmweb <itmwebhub@gmail.com>
2026-08-05 10:54:38 +0000

    Corrección 13

--- itm/ejercicios-javascript/code/estructura-rep-do-while.js
+++ itm/ejercicios-javascript/code/estructura-rep-do-while.js
@@ -1,21 +1,21 @@
 var valor;
+
 do {
-    valor=prompt("Ingrese un valor entre 0 y 999", "");
-    valor=parseInt(valor);
-    document.write("El valor "+valor+" tiene ");
-    if (valor<10) 
-    {
-    document.write("Tiene 1 dígito");
+    valor = prompt("Ingrese un valor entre 0 y 999", "");
+    valor = parseInt(valor);
+
+    document.write("El valor " + valor + " tiene ");
+
+    if (valor < 10) {
+        document.write("1 dígito");
     }
-    else
-    {
-       if (valor<100)
-        {
-         document.write("Tiene 2 dígitos");
-        }
-        else (valor<1000)
-        {
-         document.write("Tiene 3 dígitos");
-        }
+    else if (valor < 100) {
+        document.write("2 dígitos");
+    }
+    else if (valor < 1000) {
+        document.write("3 dígitos");
+    }
+
     document.write("<br>");
-} while(valor!=0);
+
+} while (valor != 0);
