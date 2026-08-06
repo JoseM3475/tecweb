fa7dfb7644d12f95a87ea35d738dd1e700d7f3b1
Mohamed Hamdaoui <mohamedfoot12345@gmail.com>
2026-08-06 08:13:32 +0000

    funciones

---
+++ mohamed/javascript/code/funciones.js
@@ -0,0 +1,25 @@
+'cuidado'
+'Ingrese su documento correctamente'
+
+'cuidado'
+'Ingrese su documento correctamente'
+
+'cuidado'
+'Ingrese su documento correctamente'
+
+document.write("Cuidado<br>");
+document.write("Ingerese su documento correctamente<br>");
+document.write("Cuidado<br>");
+document.write("Ingerese su documento correctamente<br>");
+document.write("Cuidado<br>");
+document.write("Ingerese su documento correctamente<br>");
+
+function mostrarMensaje()
+{
+document.write("Cuidado<br>");
+document.write("Ingerese su documento correctamente<br>");   
+}
+
+mostrarMensaje();
+mostrarMensaje();
+mostrarMensaje();
