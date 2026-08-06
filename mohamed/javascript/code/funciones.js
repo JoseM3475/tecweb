fdea9f10f9c25f0ff30fde8ff80e5128eaf48cf4
Mohamed Hamdaoui <mohamedfoot12345@gmail.com>
2026-08-06 10:05:27 +0000

    errata

--- mohamed/javascript/code/funciones.js
+++ mohamed/javascript/code/funciones.js
@@ -1,23 +1,13 @@
-'cuidado'
-'Ingrese su documento correctamente'
-
-'cuidado'
-'Ingrese su documento correctamente'
-
-'cuidado'
-'Ingrese su documento correctamente'
-
 document.write("Cuidado<br>");
-document.write("Ingerese su documento correctamente<br>");
+document.write("Ingrese su documento correctamente<br>");
 document.write("Cuidado<br>");
-document.write("Ingerese su documento correctamente<br>");
+document.write("Ingrese su documento correctamente<br>");
 document.write("Cuidado<br>");
-document.write("Ingerese su documento correctamente<br>");
+document.write("Ingrese su documento correctamente<br>");
 
-function mostrarMensaje()
-{
-document.write("Cuidado<br>");
-document.write("Ingerese su documento correctamente<br>");   
+function mostrarMensaje() {
+    document.write("Cuidado<br>");
+    document.write("Ingrese su documento correctamente<br>");
 }
 
 mostrarMensaje();
