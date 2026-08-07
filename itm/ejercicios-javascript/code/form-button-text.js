6e5ff3643f91753782c9fbb34d629d5311143d6b
itmweb <itmwebhub@gmail.com>
2026-08-07 11:00:03 +0000

    Sintaxis

--- itm/ejercicios-javascript/code/form-button-text.js
+++ itm/ejercicios-javascript/code/form-button-text.js
@@ -4,6 +4,6 @@ function mostrar ()
  nom=document.form1.nombre.value;
  var ed
  ed=document.form1.edad.value;
- alert("Ingreso el nombre:"+ nom);
- alert("Y la edad" + ed);
+ alert("Ingreso el nombre: "+ nom);
+ alert("Y la edad: " + ed);
  }
