45b3a1d00832e0f928421d9b0c9dd3c917269644
itmweb <itmwebhub@gmail.com>
2026-09-11 07:50:18 +0000

    Funciones estandar variables

--- itm/ejercicios-php/6-funciones/6.1.funciones.estandar/6.1.1.f.estandar.variable/codex.php
+++ itm/ejercicios-php/6-funciones/6.1.funciones.estandar/6.1.1.f.estandar.variable/codex.php
@@ -1,21 +1,52 @@
 <?php
 
-echo "Hola mundo, soy un archivo PHP apto para servidores y contengo funciones estandar para variables:<br>
-Su estructura es: <br><br>
+echo "Hola mundo, soy un archivo PHP apto para servidores y contengo varias funciones estandar que actúan sobre variables:<br>
 
-\$colores = [verde, rojo, azul];<br>
-'foreach' (\$colores as \$color){<br>
- 'echo' \$color;
-}
-<br><br>
-Un array normal tiene una serie de variables que ordena automáticamente de forma cardinal, del primero al último y asignando valores numéricos.<br>
-Permite que operemos pidiendo simplemente el lugar del valor en el índice que él mismo crea. Por ejemplo [verde,rojo,azul]:<br><br>";
+Función----------¿Qué comprueba?----------Ejemplo<br>
+empty()	         Si está vacía	          empty(\$nombre)<br>
+isset()	         Si contiene un valor	  isset(\$nombre)<br>
+is_int()	     Si es entero	          is_int(10)<br>
+is_numeric()	 Si es numérico	          is_numeric('25')<br>
+is_string()	     Si es texto	          is_string('Hola')<br>
+is_array()	     Si es un array	          is_array([1, 2])<br>";
 
-$colores = ["verde","rojo","azul"];
-foreach ($colores as $color){
-
-    echo $color. "<br>";
-}
 echo "<br>";
-echo "El array ya determina el orden de los elementos que contiene y los enumera<br>";
+
+echo "La estructura general es simple, creamos una función (function) con una variable y después le pedimos que nos devuelva algo.<br>
+Lo único ''nuevo'' es que es necesario incluir un (return) para que devuelva lo que pedimos:<br><br>
+
+// 1. Comprobar si una variable está vacía<br>
+function estaVacia(\$variable) {<br>
+    return empty(\$variable);<br>
+}<br><br>
+
+// 2. Comprobar si una variable está definida<br>
+function estaDefinida(\$variable) {<br>
+    return isset(\$variable);<br>
+}<br><br>
+
+// 3. Comprobar si una variable es un entero<br>
+function esEntero(\$variable) {<br>
+    return is_int(\$variable);<br>
+}<br><br>
+
+// 4. Comprobar si una variable es numérica<br>
+function esNumerico(\$variable) {<br>
+    return is_numeric(\$variable);<br>
+}<br><br>
+
+// 5. Comprobar si una variable es una cadena de texto<br>
+function esCadena(\$variable) {<br>
+    return is_string(\$variable);<br>
+}<br><br>
+
+// 6. Comprobar si una variable es un array<br>
+function esArray(\$variable) {<br>
+    return is_array(\$variable);<br>
+}<br><br>
+
+Importante: isset() y empty() tienen un comportamiento especial en PHP:<br> 
+No es recomendable envolver una variable que podría no existir directamente en una función propia como estaDefinida(\$variable), porque PHP intentará evaluar $variable antes de llamar a la función.<br>
+Para comprobar variables potencialmente inexistentes, usa directamente isset(\$variable) o empty(\$variable).";
+
 ?>
--- itm/ejercicios-php/6-funciones/6.1.funciones.estandar/6.1.1.f.estandar.variable/index.php
+++ itm/ejercicios-php/6-funciones/6.1.funciones.estandar/6.1.1.f.estandar.variable/index.php
@@ -2,11 +2,11 @@
 <html lang="es">
     <head>
         <meta charset="UTF-8">
-        <meta name="Función estandar variable" content="Ejercicio de funciones estandar con variables php">
-        <title>Funciones estandar variables</title>
+        <meta name="Función estandar variable varia" content="Ejercicio de funciones varias estandar con variables php">
+        <title>Varias funciones estandar variables</title>
 </head>
 <body>
-   <h1>Este es un ejercicio realizado mediante lenguaje php para mostrar funciones estandar variables en php</h1>
+   <h1>Este es un ejercicio realizado mediante lenguaje php para mostrar diferentes funciones estandar variables en php</h1>
 <br>
 <br>
     <?php include "codex.php"; ?>
